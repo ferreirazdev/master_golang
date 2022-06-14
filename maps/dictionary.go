87a7603db5de45ecb3680dc5783a0642ac498430
Flávio Ferreira <business.flavioferreira@gmail.com>
2022-06-14 05:47:57 -0300

    add word

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,19 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(dictionary map[string]string, word string) string {
-	return d[word]
+var ErrWordNotFound = errors.New("word not found")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, exist := d[word]
+	if !exist {
+		return "", ErrWordNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(key, value string) {
+	d[key] = value
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -7,16 +7,50 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "just a test"}
 
-	result := dictionary.Search(dictionary, "test")
-	expect := "just a test"
+	t.Run("known word", func(t *testing.T) {
+		result, _ := dictionary.Search("test")
+		expect := "just a test"
 
-	CompareStrings(t, result, expect)
+		CompareError(t, result, expect)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+
+		if err != nil {
+			t.Fatal("expected to get an error.")
+		}
+	})
 }
 
-func CompareStrings(t *testing.T, result, expect string) {
+func CompareError(t *testing.T, result, expect string) {
 	t.Helper()
 
 	if result != expect {
 		t.Errorf("resultado '%s', esperado '%s', dado '%s'", result, expect, "test")
 	}
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	key := "test"
+	value := "just a test"
+
+	dictionary.Add(key, value)
+
+	compareValue(t, dictionary, key, value)
+}
+
+func compareValue(t *testing.T, dictionary Dictionary, key, value string) {
+	t.Helper()
+
+	result, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("word not found", err)
+	}
+
+	if result != value {
+		t.Errorf("resultado '%s', esperado '%s'", result, value)
+	}
+
 }
